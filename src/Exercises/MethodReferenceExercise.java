3b597ea813740b83392e58aa9ea01ad768f30555
Alexs Ruiz <ralexs.acu@gmail.com>
2024-09-16 22:26:17 -0600

    MethodReference Exercise

---
+++ src/Exercises/MethodReferenceExercise.java
@@ -0,0 +1,54 @@
+package Exercises;
+
+public class MethodReferenceExercise {
+    public static void main(String[] args) {
+        /**
+         * Exercise 1:
+         *
+         * Create a method in a class that takes a string and prints it in lowercase.
+         * Use a method reference to pass this method to a Consumer<String>.
+         *
+         */
+
+        // Your Code Here...
+
+        /**
+         * Exercise 2:
+         *
+         * Write a static method that prints the square of an integer.
+         * Use a method reference to pass this static method to a Consumer<Integer>.
+         *
+         */
+
+        // Your Code Here...
+
+        /**
+         * Exercise 3:
+         *
+         * Create a method that prints the length of a string.
+         * Use a method reference to pass this method to a Consumer<String>.
+         *
+         */
+
+        // Your Code Here...
+
+        /**
+         *  Exercise 4:
+         *
+         * Create a class with a non-static method that prints the first character of a string.
+         * Use a method reference to pass this non-static method to a Consumer<String>.
+         *
+         */
+
+        // Your Code Here...
+
+        /**
+         * Exercise 5:
+         *
+         * Create a method in a class that takes a list of strings and prints each string in uppercase.
+         * Use method reference to pass this method to a Consumer<List<String>>.
+         */
+
+        // Your Code Here...
+    }
+}
