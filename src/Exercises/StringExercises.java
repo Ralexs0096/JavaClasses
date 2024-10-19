3914e4be19d5e275da3c6adfcc0797d9c2f68601
Alexs Ruiz <ralexs.acu@gmail.com>
2024-10-18 18:57:11 -0600

    String Methods Exercise

---
+++ src/Exercises/StringExercises.java
@@ -0,0 +1,62 @@
+
+package Exercises;
+
+public class StringExercises {
+    public static void main(String[] args) {
+        /**
+         * Challenge 1: Word Counter
+         * Create a program that takes a sentence as input and counts the number
+         * of words in it. The sentence may contain multiple spaces between words.
+         *
+         * Your task is to ensure the word count is accurate.
+         *
+         * input:
+         *
+         * String frase = "Java es un lenguaje de programación poderoso";
+         *
+         * output: 7
+         */
+
+        // Your Code HERE:
+
+        /**
+         * Challenge 2: Vowel Replacer
+         * Write a program that replaces all the vowels in a given string
+         * with a special character of your choice.
+         *
+         * Ensure that the program works with both uppercase and lowercase vowels.
+         *
+         * input:
+         *
+         * String text = "Learning Java is fun";
+         *
+         * output: (example using *)
+         *
+         * L**rn*ng J*v* *s f*n
+         */
+
+        // Your Code HERE:
+
+        /**
+         * Challenge 3: Palindrome Validator
+         * Design a program that checks if a given string is a palindrome.
+         * The program should ignore spaces, punctuation,
+         * and capitalization when determining whether
+         * the string reads the same forwards and backwards.
+         *
+         * input:
+         *
+         * String original = "A man a plan a canal Panama"
+         *
+         * output:
+         *
+         * It's a palindrome
+         *
+         * or
+         *
+         * It's not a palindrome
+         */
+
+        // Your Code HERE:
+    }
+}
