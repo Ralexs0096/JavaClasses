1b57e214ecbe7047099bc2724b916e3839cb5f69
Alexs Ruiz <ralexs.acu@gmail.com>
2024-10-28 21:45:00 -0600

    File write example

--- src/FileCreation.java
+++ src/FileCreation.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.IOException;
+import java.io.FileWriter;
 
 public class FileCreation {
     public static void main(String[] args) {
@@ -9,9 +10,20 @@ public class FileCreation {
        try {
            File myFile = new File("/Users/ralexs/Desktop/Dev/nahum.txt");
            myFile.createNewFile();
-           System.out.println("File created " + myFile.getName());
+
+           if(myFile.exists()){
+               System.out.println("File already exists");
+
+               FileWriter myWriter = new FileWriter("/Users/ralexs/Desktop/Dev/nahum.txt");
+               myWriter.write("Files in java might be tricky, but it is fun enough!");
+               myWriter.close();
+               System.out.println("Successfully wrote to the file");
+           } else {
+            System.out.println("File created " + myFile.getName());
+           }
        } catch (IOException e){
            System.out.println("File not created");
+           e.printStackTrace();
        }
     }
 }
