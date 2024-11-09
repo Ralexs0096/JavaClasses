1b11c50b97bdac866989f4d64d78668ced9c5da0
Alexs Ruiz <ralexs.acu@gmail.com>
2024-11-08 21:21:06 -0600

    Delete an existing file

--- src/MultipleLinesClass.java
+++ src/MultipleLinesClass.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -6,23 +7,8 @@ import java.util.List;
 
 public class MultipleLinesClass {
     public static void main(String[] args) {
-        Path pathFile = Paths.get("test.txt");
-        try {
-            List<String> lines = Files.readAllLines(pathFile);
-
-            // [0,1,2...]
-
-            for (int i = 0; i < lines.size(); i++) {
-                if( lines.get(i).contains("comen trigo") ) {
-                    lines.set(i, lines.get(i).replace("comen trigo", "Java is great!"));
-                }
-            }
-
-            // override the file with the new value
-            Files.write(pathFile, lines);
-            System.out.println("File updated successfully");
-        } catch (IOException e) {
-            System.err.println(e.getMessage());
-        }
+        // to delete an existing file. Don't require a try/catch statement
+           File newFile = new File("test.txt");
+           newFile.delete();
     }
 }
