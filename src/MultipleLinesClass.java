2e87299bf65668d52d377f52166cc8736ee61da7
Alexs Ruiz <ralexs.acu@gmail.com>
2024-11-08 20:59:41 -0600

    Update file with append true

--- src/MultipleLinesClass.java
+++ src/MultipleLinesClass.java
@@ -1,52 +1,20 @@
-import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class MultipleLinesClass {
     public static void main(String[] args) {
-        /**
-         * Please type a number (given number)
-         *
-         * generate table of the given number
-         *
-         * -- print the table on the console (first commit)
-         * -- create a new file with the name: "Table-<given number>" (second commit)
-         * -- Write the table on the created table-given-number file (third commit)
-         *
-         * note: the table contains the values from 1 to 12
-         *
-         *    7 * 1 = 7
-         *    7 * 2 = 14
-         *    7 * 3 = 21
-         *    ...
-         *    7 * 12 = 84
-         */
 
         var keyboard = new Scanner(System.in);
-        System.out.println("Hey! Type a number: ");
-        int givenNumber = keyboard.nextInt();
+        System.out.println("Type a message: ");
+        String message = keyboard.nextLine();
 
-        var tableValues = new ArrayList<String>();
+        try (FileWriter fileWriter = new  FileWriter("test.txt", true)) {
 
-        String tableName = "Table-" + givenNumber + ".txt";
-        Path filePath = Paths.get(tableName);
-
-        for (int i = 1; i < 13; i++) {
-            tableValues.add(givenNumber + " * " + i + " = " + (givenNumber * i));
-            System.out.println(givenNumber + " * " + i + " = " + (givenNumber * i));
-        }
-
-        tableValues.forEach();
-
-        try {
-            Files.write(filePath, tableValues);
-            System.out.println("File Created");
+           fileWriter.write("\n" + message);
+            System.out.println("File Updated with message: " + message);
         } catch (IOException e) {
-            System.err.println(e);
+            System.err.println(e.getMessage());
         }
     }
 }
