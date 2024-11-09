9653965410acc033031b9f852b9f6d699116a329
Alexs Ruiz <ralexs.acu@gmail.com>
2024-11-08 21:26:03 -0600

    Delete a file using the static method of Files

--- src/MultipleLinesClass.java
+++ src/MultipleLinesClass.java
@@ -7,8 +7,14 @@ import java.util.List;
 
 public class MultipleLinesClass {
     public static void main(String[] args) {
-        // to delete an existing file. Don't require a try/catch statement
-           File newFile = new File("test.txt");
-           newFile.delete();
+       Path filePath = Paths.get("Table-5.txt");
+
+       try {
+           Files.delete(filePath);
+           System.out.println("File deleted");
+       } catch (IOException e){
+           System.err.println(e.getMessage());
+       }
+
     }
 }
