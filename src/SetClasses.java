a8e8307b1a8389c612cc486f64459d60d19e18da
Alexs Ruiz <ralexs.acu@gmail.com>
2024-11-25 20:27:35 -0600

    LinkedHashSet example using Integers

--- src/SetClasses.java
+++ src/SetClasses.java
@@ -3,13 +3,14 @@ import java.util.Set;
 
 public class SetClasses {
     public static void main(String[] args) {
-        Set<String> linkedHashSet = new LinkedHashSet<>();
+        Set<Integer> linkedHashSet = new LinkedHashSet<>();
 
-        linkedHashSet.add("Apple");
-        linkedHashSet.add("Banana");
-        linkedHashSet.add("Cherry");
-        linkedHashSet.add("Mango");
-        linkedHashSet.add("Pear");
+        linkedHashSet.add(12);
+        linkedHashSet.add(56);
+        linkedHashSet.add(78);
+        linkedHashSet.add(3);
+        linkedHashSet.add(2);
+        linkedHashSet.add(12);
         // the order is preserved
 
         linkedHashSet.forEach(System.out::println);
