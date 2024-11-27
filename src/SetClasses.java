732c38a093b6955adc8599e6e9ad3a22b6473aff
Jose3708k <josenahumacunalopez8@gmail.com>
2024-11-26 19:36:05 -0600

    Set exericise creating a lotery

--- src/SetClasses.java
+++ src/SetClasses.java
@@ -1,6 +1,5 @@
 
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 public class SetClasses {
     public static void main(String[] args) {
@@ -12,6 +11,34 @@ public class SetClasses {
          * the award is up to you
          */
 
-        // TODO: your code here
+        Set<String> participants = new HashSet<>();
+
+        participants.add("Pedro");
+        participants.add("Juan");
+        participants.add("Carlos");
+        participants.add("Jenny");
+        participants.add("Alex");
+        participants.add("Nahum");
+
+        LinkedHashSet<String> awards = new LinkedHashSet<>();
+        awards.add("Car");
+        awards.add("Motorcycle");
+        awards.add("Blender");
+        awards.add("Fan");
+        awards.add("CellPhone");
+        awards.add("Televise");
+        
+
+        List<String> participantList = new ArrayList<>(participants);
+        List<String> awardList = new ArrayList<>(awards);
+
+
+        String winner = participantList.get((int) (Math.random() * participantList.size()));
+        String award = awardList.get((int) (Math.random() * awardList.size()));
+
+
+        System.out.println("The winner is: " + winner);
+        System.out.println("The award is: " + award);
+
     }
 }
