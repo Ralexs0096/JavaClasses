5c38cb88614d3a061400d661711a2c0771cfd96c
Alexs Ruiz <ralexs.acu@gmail.com>
2024-11-29 21:20:59 -0600

    Add new class User using enumSet

--- src/EnumSetClass.java
+++ src/EnumSetClass.java
@@ -4,16 +4,37 @@ enum Permission {
     READ, WRITE, EXECUTE, DELETE
 }
 
+class User {
+    private String username;
+    private EnumSet<Permission> permissions;
+
+    // constructor
+    public User(String username) {
+        this.username = username;
+        this.permissions = EnumSet.noneOf(Permission.class);
+    }
+
+    public void grantPermission(Permission permission) {
+        this.permissions.add(permission);
+        System.out.println("Granted " + permission + " to " + username);
+    }
+
+    public void revokePermission(Permission permission) {
+        this.permissions.remove(permission);
+        System.out.println("Revoked " + permission + " to " + username);
+    }
+
+    public boolean hasPermission(Permission permission) {
+        return this.permissions.contains(permission);
+    }
+
+    public void showPermissions(Permission permission) {
+        System.out.println(username + "'s permissions: " + this.permissions);
+    }
+}
+
 public class EnumSetClass {
     public static void main(String[] args) {
-        EnumSet<Permission> userPermissions = EnumSet.of(Permission.READ, Permission.EXECUTE);
 
-        System.out.println(userPermissions.contains(Permission.DELETE)); // false
-
-        userPermissions.add(Permission.WRITE);
-        System.out.println(userPermissions); // [READ, WRITE, EXECUTE]
-
-        userPermissions.remove(Permission.READ);
-        System.out.println(userPermissions); // [WRITE, EXECUTE]
     }
 }
