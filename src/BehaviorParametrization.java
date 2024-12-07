64039fd88b3829605776c38f9068f33f01f05d64
Alexs Ruiz <ralexs.acu@gmail.com>
2024-12-06 20:50:21 -0600

    Behavioral parametrization - 2

--- src/BehaviorParametrization.java
+++ src/BehaviorParametrization.java
@@ -15,8 +15,8 @@ public class BehaviorParametrization {
     public static void main(String[] args) {
         ArrayList<Apple> inventory = new ArrayList<>();
 
-        var result = filterGreenApples(inventory);
-        
+        var result = filterRedApples(inventory);
+
     }
 
     static List<Apple> filterGreenApples(List<Apple> inventory) {
@@ -24,6 +24,18 @@ public class BehaviorParametrization {
 
         for (Apple apple : inventory) {
             if (Color.GREEN.equals(apple.getColor())) {
+                greenApples.add(apple);
+            }
+        }
+
+        return greenApples;
+    }
+
+    static List<Apple> filterRedApples(List<Apple> inventory) {
+        List<Apple> greenApples = new ArrayList<>();
+
+        for (Apple apple : inventory) {
+            if (Color.RED.equals(apple.getColor())) {
                 greenApples.add(apple);
             }
         }
