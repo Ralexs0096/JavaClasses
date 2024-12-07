26d3fc114b8367f60b7e26b2865592eac7ee85f8
Alexs Ruiz <ralexs.acu@gmail.com>
2024-12-06 20:49:02 -0600

    Behavioral parametrization - 1

---
+++ src/BehaviorParametrization.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
+enum Color {
+    RED, GREEN
+}
+
+class Apple {
+    public Color getColor() {
+        return Color.RED;
+    }
+}
+
+public class BehaviorParametrization {
+    public static void main(String[] args) {
+        ArrayList<Apple> inventory = new ArrayList<>();
+
+        var result = filterGreenApples(inventory);
+        
+    }
+
+    static List<Apple> filterGreenApples(List<Apple> inventory) {
+        List<Apple> greenApples = new ArrayList<>();
+
+        for (Apple apple : inventory) {
+            if (Color.GREEN.equals(apple.getColor())) {
+                greenApples.add(apple);
+            }
+        }
+
+        return greenApples;
+    }
+}
