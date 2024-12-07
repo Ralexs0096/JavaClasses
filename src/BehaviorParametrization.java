c5871eb4bd2b47c917cb03b9bc33721f975e09a7
Alexs Ruiz <ralexs.acu@gmail.com>
2024-12-06 21:03:00 -0600

    Behavioral parametrization - 4

--- src/BehaviorParametrization.java
+++ src/BehaviorParametrization.java
@@ -15,43 +15,44 @@ class Apple {
     }
 }
 
+interface ApplePredicate {
+    boolean test(Apple apple);
+}
+
+class AppleGreenColorPredicate implements ApplePredicate {
+    public boolean test(Apple apple) {
+        return Color.GREEN.equals(apple.getColor());
+    }
+}
+
+class AppleRedColorPredicate implements ApplePredicate {
+    public boolean test(Apple apple) {
+        return Color.RED.equals(apple.getColor());
+    }
+}
+
+class AppleHeavyPredicate implements ApplePredicate {
+    public boolean test(Apple apple) {
+        return apple.getWeight() > 150;
+    }
+}
+
 public class BehaviorParametrization {
     public static void main(String[] args) {
         ArrayList<Apple> inventory = new ArrayList<>();
 
-        var result = filterRedApples(inventory);
+        var greenApples = filterApples(inventory, new AppleGreenColorPredicate());
+        var redApples = filterApples(inventory, new AppleRedColorPredicate());
+        var heavyApples = filterApples(inventory, new AppleHeavyPredicate());
 
     }
 
-    static List<Apple> filterGreenApples(List<Apple> inventory) {
-        List<Apple> greenApples = new ArrayList<>();
-
-        for (Apple apple : inventory) {
-            if (Color.GREEN.equals(apple.getColor())) {
-                greenApples.add(apple);
-            }
-        }
-
-        return greenApples;
-    }
-
-    static List<Apple> filterRedApples(List<Apple> inventory) {
-        List<Apple> redApple = new ArrayList<>();
-
-        for (Apple apple : inventory) {
-            if (Color.RED.equals(apple.getColor())) {
-                redApple.add(apple);
-            }
-        }
-
-        return redApple;
-    }
-
-    static List<Apple> filterApplesByWeight(List<Apple> inventory, int weight) {
+    // DRY - Don't Repeat Yourself
+    static List<Apple> filterApples(List<Apple> inventory, ApplePredicate p) {
         List<Apple> result = new ArrayList<>();
 
         for (Apple apple : inventory) {
-            if (apple.getWeight() > weight) {
+            if (p.test(apple)) {
                 result.add(apple);
             }
         }
